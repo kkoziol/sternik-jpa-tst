027b790bbaebece3c107fc8d7d28eb9ecf559e99
Krzysiek Koziol <kkoziol@gmail.com>
2018-02-18 16:57:48 +0100

    Pierwszy select z bazy

--- src/main/java/dbAccess.java
+++ src/main/java/dbAccess.java
@@ -10,7 +10,30 @@ import javax.sql.rowset.RowSetFactory;
 import javax.sql.rowset.RowSetProvider;
 
 public class dbAccess {
-	public static void main(String[] args) throws SQLException {
-		
-	}
+    public static void main(String[] args) throws SQLException {
+        Connection conn = null;
+        try {
+            Class.forName("org.h2.Driver");
+
+            String database = "jdbc:h2:~/test";
+
+            conn = DriverManager.getConnection(database, "sa", "sa");
+
+             Statement st = conn.createStatement();
+             ResultSet res = st.executeQuery("SELECT id, name, desc FROM article");
+
+             while (res.next()) {
+                 int id = res.getInt(1);
+                 String name = res.getString(2);
+                 String desc = res.getString(3);
+                 System.out.println("" + id + " " + name + " " + desc);
+             }
+             st.close();
+            conn.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            conn.close();
+        }
+    }
 }
