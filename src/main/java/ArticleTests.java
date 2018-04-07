44ef543a856caef9056616b1d27a893116f021ca
Krzysiek Koziol <kkoziol@gmail.com>
2018-04-07 15:06:03 +0200

    Zapis i odczyt

--- src/main/java/ArticleTests.java
+++ src/main/java/ArticleTests.java
@@ -11,15 +11,70 @@ public class ArticleTests {
 
     public static void main(String[] args) throws IOException {
 
+        int id;
+        
         EntityManager em = null;
         try {
             EntityManagerFactory emf = Persistence.createEntityManagerFactory("h2-eclipselink");
             em = emf.createEntityManager();
- 
+
+            em.getTransaction().begin();
+
+            // UserDetails user = em.find(UserDetails.class, "system30.8906986756300783");
+
+            Article article = new Article();
+            // article.setId(1 + (int)(Math.random()*100));
+            article.setName("ram");
+            article.setPrice(100);
+            article.setDescription("DDR3 16GB");
+            // article.setUser(user);
+
+            em.persist(article);
+
+            id=article.getId();
+            System.out.println("Article dostało ID="+ id);
+            
+            article = new Article();
+            article.setName("CDROM");
+            article.setPrice(50);
+            article.setDescription("x120");
+            em.persist(article);
+            em.getTransaction().commit();
+            
+            em.close();
+            System.out.println("Persisted");
+
+            System.out.println("kliknij enter");
+            System.in.read();
+
+            em = emf.createEntityManager();
+            em.getTransaction().begin(); 
+            
+            Article article2 = em.find(Article.class, id);
+            System.out.println("Wyciagniete z H2="+article2);
+            
+            em.getTransaction().commit();
+
+            em.close();
+            System.out.println("A teraz bedze kasowanie");
+
+            System.out.println("kliknij enter");
+            System.in.read();
+
+            em = emf.createEntityManager();
+            em.getTransaction().begin(); 
+            
+            article2 = em.find(Article.class, id);
+            System.out.println("Wyciagniete z H2="+article2);
+            
+            em.remove(article2);
+            
+            em.getTransaction().commit();
+
             
         } finally {
             if (em != null && em.isOpen()) {
-                System.out.println("hi");
+                System.out.println("EM close");
                 em.close();
             }
         }
